cc4826ae5adb88ac0186518ee5d1e78aaff7c4ad
worakamonwmt <worakamon.wmt@gmail.com>
2024-07-18 16:33:00 +0700

    แก้responsive

--- index.js
+++ index.js
@@ -29,10 +29,3 @@ function scrollFunction() {
     navbar.style.color = "white";
   }
 }
-
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
-
