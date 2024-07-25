f07be72497354804279bde00b166ee32cdc2276a
worakamonwmt <worakamon.wmt@gmail.com>
2024-07-25 14:50:07 +0700

    แก้ responsive

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-var timeDisplay = document.getElementById("time");
-
-
-function refreshTime() {
-  var dateString = new Date().toLocaleString("en-US", { timeZone: "Asia/Bangkok" });
-  var formattedString = dateString.replace(", ", " - ");
-  timeDisplay.innerHTML = formattedString;
-}
-
-setInterval(refreshTime, 1000);
 
 window.onscroll = function() {
   scrollFunction();
